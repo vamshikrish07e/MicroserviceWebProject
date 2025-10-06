eeba4e65dbb5921d26fbb2cc16f3ed791243d1da
admin <admin@DESKTOP-UIFBAUK>
2025-10-06 10:04:51 +0530

    test

--- eureka-service-registry/src/main/java/com/productapp/EurekaServiceRegistryApplication.java
+++ eureka-service-registry/src/main/java/com/productapp/EurekaServiceRegistryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServiceRegistryApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
 	}
 
